b1cab5f215891b5d32f32e0eb29827f5f5ad37ab
Brian Hasal <brian.hasal@gmail.com>
2022-05-09 18:30:31 -0700

    Final

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/users/" => "users#show"
+  post "/users" => "users#create"
+  patch "/users/:id" => "users#update"
+
+
 end
